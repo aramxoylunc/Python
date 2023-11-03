0db2b0af9fe08c07b3ca5f9871eb5105a6747f70
aramxoylunc <144222735+aramxoylunc@users.noreply.github.com>
2023-11-03 15:13:04 +0400

    Lec_9 main.py final change

--- lec_9/main.py
+++ lec_9/main.py
@@ -22,11 +22,33 @@ class matrix :
             for j in range(self._m) :
                 result._matrix[i][j] = mat2._matrix[i][j] + self._matrix[i][j]
         return result
+        
+    def __sub__ (self, mat2) :
+        if mat2._n != self._n or mat2._m != self._m :
+            return None
+        result = matrix(self._n,self._m)
+        for i in range(self._n) :
+            for j in range(self._m) :
+                result._matrix[i][j] = self._matrix[i][j] - mat2._matrix[i][j]
+        return rresul
+        
+    def __mul__ (self, mat2) :
+        if self._m != mat2._n :
+            return None
+        result = matrix(self._n,mat2._m)
+        for i in range(self._n) :
+            for j in range(mat2._m) :
+                temp = 0
+                for k in range(self._m) :
+                    temp += self._matrix[i][k] * mat2._matrix[k][j]
+                result._matrix[i][j] = temp
+        return result
 
-obj1 = matrix(3,4)
-obj2 = matrix(3,4)
+obj1 = matrix(1,2)
+obj2 = matrix(2,3)
 
 print(obj1)
 print(obj2)
+print(obj1*obj2)
 print(obj1+obj2)
 
